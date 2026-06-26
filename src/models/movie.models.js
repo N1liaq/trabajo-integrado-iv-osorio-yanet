d8f1e40038e1b2119d4a468a0fa1d712eafeddbf
N1liaq_ <ailinyanos83@gmail.com>
2026-06-26 00:51:23 -0300

    se definió los valores de las tablas y se uso Sequelizeen <feature/crud-peliculas>

---
+++ src/models/movie.models.js
@@ -0,0 +1,37 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+
+export const Movie = sequelize.define(
+  "Movie",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    synopsis: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "movies",
+    timestamps: false,
+  },
+);
