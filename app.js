5de80373da1479847745a99dad2e4f11ec0f2335
N1liaq_ <ailinyanos83@gmail.com>
2026-06-26 01:40:55 -0300

    se actualizó <app.js> las conexiones con la base de datos y las rutas en la rama <feature/crud-peliculas>

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 import express from "express";
+import sequelize from "./src/config/database.js";
+import movieRoutes from "./src/routes/movie.routes.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+// Rutas
+app.use("/api/movies", movieRoutes);
+
+// Sincronizar base de datos e iniciar servidor
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("Base de datos sincronizada correctamente.");
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error al conectar con la base de datos:", error);
+  });
