3e34c20b274cd2af18902395da8b181133489249
N1liaq_ <ailinyanos83@gmail.com>
2026-06-26 02:28:54 -0300

    se arreglaron errores en el código en <feature/crud-peliculas>

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("movies", "root", "' '", {
+const sequelize = new Sequelize("movies", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
